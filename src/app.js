19a2c08c614f37833ef671c1592c98a4e27743ac
= <=>
2024-05-20 10:29:59 +0530

    fourth commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
-// so here in this project we use nodemon it is a package which reloads the server after the file is saved so that we dont need to stop and restart it
-
-import express from 'express'
+import express from 'express';
 import cookieParser from 'cookie-parser';
-import cors from "cors"
+import cors from 'cors';
+import router from './routes/userRoutes.js';
+
 const app = express();
 
-// app.use() we use this methods for the configurations and for the middle wares
 app.use(cors({
-    origin:process.env.CORS_ORIGIN, //this points {kaha kaha se request aa sakti hai}
-    credentials:true
-}))
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
+// ROUTES DECLARATION
+app.use('/users', router);
 
+// URL Example:
+// http://localhost:8000/users/register
 
-export { app }
+export { app };
