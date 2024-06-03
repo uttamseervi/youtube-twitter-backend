ec60b493193ea7b0461fe43217ce64695a5cfa82
= <=>
2024-06-03 17:29:28 +0530

    got the subscribers and subscribed to controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -235,13 +235,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200)
-        .json(200, req.user, "Current User fetched Successfully")
+        .json(new apiResponse(200, req.user, "Current User fetched Successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
     const { fullName, email } = req.body;
     if (!fullName || !email) throw new apiError(400, "All fields are required");
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -283,6 +283,68 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     return res.status(200)
         .json(new apiResponse(200, user, "AVATAR UPDATED SUCCESSFULLY"))
 })
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+    const { username } = req.params;
+    if (!username?.trim()) throw new apiError(400, "USERNAME IS MISSING");
+    await User.find({ username })
+
+    const channel = User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: { $size: "$subscribers" } /*WE USE DOLLER SYMBOL JUST TO DEMONSTRATE IT AS A FIELD*/,
+                channelSubscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    cond: {
+                        /*cond {condition has three parameters one is if agar true hai to then warna else}*/
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"], then: true, else: false } /*we use in to check whether the user exist in it or not it will work for both arrays and objects z*/
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName:1,
+                subscribersCount:1,
+                channelSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+
+            }/*what will project do????>>>  it will project the fields that we want to see in the result*/ /*we use project to remove the fields that we dont want to see in the result*/
+
+
+        }
+    ])
+    if(!channel?.length) throw new apiError(400,"CHANNEL DON'T EXIST");
+    return res
+    .status(200)
+    .json(new apiResponse(200,channel[0],"USER CHANNEL FETCHED SUCCESSFULLY"))
+})
+
 
 
 
